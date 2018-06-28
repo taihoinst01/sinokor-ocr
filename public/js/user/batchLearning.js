92c360924c1a474a411080008c124697b2b3fa1b
JeongHanYoung <jhy2089@naver.com>
2018-06-28 17:16:25 +0900

    imageMagick 모듈 추가 , tif to jpg router 추가 , 파일 다중업로드 , 외부 라이브러리(form 비동기) 추가

--- public/js/user/batchLearning.js
+++ public/js/user/batchLearning.js
@@ -1,30 +1,26 @@
 ﻿$(function () {
-    $('.uploadBtn').click(function () {
-        $(this).next().click();
-    });
-
-    $('#fileuploadBtn').click(function () {
-        $(this).next().click();
-    });
-
-    $('#fileupload').fileupload({
-        url: '/batchLearning/upload',
-        dataType: 'json',
-        add: function (e, data) {
-            data.context = $('<p/>').text('Uploading...').appendTo(document.body);
-            data.submit();
-        },
-        done: function (e, data) {
-            alert('upload finished');
-        },
-        progressall: function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            $('#progress .progress-bar').css(
-                'width',
-                progress + '%'
-            );
+    $('#uploadFile').change(function () {
+        if ($(this).val() !== '') {
+            $('#multiUploadForm').submit();
         }
     });
+    
+    $('#multiUploadBtn').click(function () {     
+        $('#uploadFile').click();
+    });
+    
+    $('#multiUploadForm').ajaxForm({
+        beforeSubmit: function (data, frm, opt) {
+            return true;
+        },
+        success: function (responseText, statusText) {
+            alert("전송성공!!");
+        },
+        error: function (e) {
+            console.log(e);
+        }
+    });
+    
 })
 
 
--- routes/user/batchLearning.js
+++ routes/user/batchLearning.js
@@ -4,6 +4,17 @@ var fs = require('fs');
 var multer = require("multer");
 var exceljs = require('exceljs');
 var appRoot = require('app-root-path').path;
+var exec = require('child_process').exec;
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    }),
+});
 var router = express.Router();
 
 router.get('/favicon.ico', function (req, res) {
@@ -21,9 +32,23 @@ router.post('/', function (req, res) {
 });
 
 // fileupload
-router.post('/upload', function (req, res) {
-    console.log('post');
-    res.end();
+router.post('/multiUpload', upload.any(), function (req, res) {
+    var file = req.files;
+ 
+    console.log(file);
+    res.send();
+});
+
+// tif to jpg 변환
+router.post('/tif', function (req, res) {
+    var ifile = appRoot + '\\uploads\\test.tif';
+    var ofile = appRoot + '\\pdf2img\\test.jpg'
+
+    exec('module\\imageMagick\\convert.exe -density 600x600 ' + ifile + ' ' + ofile, function (err, out, code) {
+        console.log(out)
+    });
+
+    res.send({});
 });
 
 module.exports = router;
