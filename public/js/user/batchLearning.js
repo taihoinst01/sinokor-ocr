ce3be989aaa14a4d744eaee1270f074ec43119d5
gjlim <gjlim@taihoinst.onmicrosoft.com>
2018-08-18 11:13:01 +0900

    계약명 commit;

--- public/js/user/batchLearning.js
+++ public/js/user/batchLearning.js
@@ -1049,7 +1049,7 @@ var searchBatchLearnDataList = function (addCond) {
                         <td><a onclick="javascript:fn_viewImageData('${entry.ORIGINFILENAME}', this)" href="javascript:void(0);">${nvl(entry.ORIGINFILENAME)}</a></td> <!--파일명-->
                         <td>${nvl(entry.STATUS)}</td> <!--학습여부-->
                         <td>${nvl(entry.OGCOMPANYNAME)}</td> <!--추출 출재사명-->
-                        <td></td> <!--추출 계약명-->
+                        <td>${nvl(entry.CTNM)}</td> <!--추출 계약명-->
                         <td>${nvl(entry.OGCONTRACTNAME)}</td> <!--출재사명 원본-->
                         <td>${nvl(entry.CONTRACTNAMESUMMARY)}</td> <!--계약명 원본-->
                         <td>${nvl(entry.UY)}</td> <!--UY-->
