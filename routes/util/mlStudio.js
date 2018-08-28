ca39608e86b4e6c4f626c381788c6bffd30e9b72
JeongHanYoung <jhy2089@naver.com>
2018-08-28 10:04:47 +0900

    Azure ml studio train (proxy 서버로 데이터 전송 후 csv 변환 저장)

--- routes/util/mlStudio.js
+++ routes/util/mlStudio.js
@@ -7,20 +7,51 @@ var fs = require('fs');
 var request = require("request");
 var propertiesConfig = require(appRoot + '/config/propertiesConfig.js');
 var commonUtil = require(appRoot + '/public/js/common.util.js');
-var request = require('sync-request');
 var sync = require('./sync.js');
 
 
-exports.train = function (req, type, done) {
+exports.train = function (req, done) {
     return new Promise(async function (resolve, reject) {
         try {
+            var ogCompanySid = [];
+            var ctnmSid = [];
             var formData = {
-                data: [],
-                type: type
+                flmdata: [],
+                fmData: [],
+                cmData: []
             };
-            for (var i in req) {
-                data.push({ 'data' : req[i].sid, 'class' : req[i].colLbl });
+
+            for (var i in req.data) {
+                var item = req.data[i];
+                var classNum = 3;
+                if (item.colLbl == 0) {
+                    classNum = 1;
+                    ogCompanySid.push(item.sid);
+                } else if (item.colLbl == 1) {
+                    classNum = 2;
+                    ctnmSid.push(item.sid);
+                }
+                formData.flmdata.push({ 'data': item.sid, 'class': classNum });
+                formData.cmData.push({ 'data': req.docCategory.DOCTYPE + ',' + item.sid, 'class': item.colLbl });
+            }                            
+
+            if (ogCompanySid && ctnmSid) {
+                if (ogCompanySid.length == 1 && ctnmSid.length == 1) { // 1:1
+                    formData.fmData.push({ 'data': ogCompanySid[0] + ',' + ctnmSid[0], 'class': req.docCategory.DOCTYPE });
+                } else if (ogCompanySid.length == 1 && ctnmSid.length > 1) { // 1:N
+                    for (var i in ctnmSid) {
+                        formData.fmData.push({ 'data': ogCompanySid[0] + ',' + ctnmSid[i], 'class': req.docCategory.DOCTYPE });
+                    }
+                } else if (ogCompanySid.length > 1 && ctnmSid.length == 1) { // N;1
+                    for (var i in ogCompanySid) {
+                        formData.fmData.push({ 'data': ogCompanySid[i] + ',' + ctnmSid[0], 'class': req.docCategory.DOCTYPE });
+                    }
+                }
+            } else {
+                formData.fmData = [];
             }
+
+
             request.post('http://localhost:3001/ml/train', { json: true, body: formData }, function (err, httpRes, body) {
                 return done(null, body);
             });
@@ -33,13 +64,7 @@ exports.train = function (req, type, done) {
         } catch (err) { 
             reject(err);
         } finally {
-            if (conn) {
-                try {
-                    await conn.release();
-                } catch (e) {
-                    console.error(e);
-                }
-            }
+            
         }
     });
 };
