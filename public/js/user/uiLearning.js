e23046cd147de06946b8131b0f016c0f81020182
JeongHanYoung <jhy2089@naver.com>
2018-09-03 15:27:32 +0900

    commit

--- public/js/user/uiLearning.js
+++ public/js/user/uiLearning.js
@@ -199,9 +199,9 @@ function processImage(fileName) {
             addProgressBar(31, 40);
             appendOcrData(fileName, data.regions);
         } else if (data.error) { //ocr 이외 에러이면
-            endProgressBar();
-            alert(data.error);
-            location.href = '/uiLearning';
+            //endProgressBar();
+            //alert(data.error);
+            //location.href = '/uiLearning';
         } else { // ocr 에러 이면
             insertCommError(data.code, 'ocr');
             endProgressBar();
