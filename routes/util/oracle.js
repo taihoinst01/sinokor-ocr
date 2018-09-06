c062b0272fa619bc3e347b1ab5246e9941aa1d53
JeongHanYoung <jhy2089@naver.com>
2018-09-06 13:25:55 +0900

    modify select after training list & modify insert NotInvoice batch_learn_list

--- routes/util/oracle.js
+++ routes/util/oracle.js
@@ -510,6 +510,12 @@ exports.selectBatchLearnList = function (req, done) {
         var res = [];
         let conn;
         let colNameArr = ['SEQNUM', 'FILEPATH', 'ORIGINFILENAME'];
+        var condQuery
+        if (!commonUtil.isNull(req.body.addCond)) {
+            if (req.body.addCond == "LEARN_N") condQuery = "(L.STATUS != 'D' OR L.STATUS IS NULL)";
+            else if (req.body.addCond == "LEARN_Y") condQuery = "(L.STATUS = 'D')";
+        }
+
         try {
             conn = await oracledb.getConnection(dbConfig);          
             var rowNum = req.body.moreNum;
@@ -518,7 +524,7 @@ exports.selectBatchLearnList = function (req, done) {
                                                       TBL_BATCH_ANSWER_FILE F 
                                                       LEFT OUTER JOIN TBL_BATCH_LEARN_LIST L 
                                                       ON F.FILEPATH = L.FILEPATH 
-                                                    WHERE (L.STATUS != 'D' OR L.STATUS IS NULL) 
+                                                    WHERE ` + condQuery + `
                                                     AND F.FILEPATH LIKE '/2018/%' 
                                                     AND ROWNUM <= :num
                                                     ORDER BY F.IMGID ASC `, [req.body.moreNum]);
