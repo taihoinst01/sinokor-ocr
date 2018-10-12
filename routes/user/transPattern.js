03d03f0663c0c375be401ff1cad0bbcd32429196
hskim <hskim@taihoinst.com>
2018-10-12 14:03:55 +0900

    transpattern edit hskim

--- routes/user/transPattern.js
+++ routes/user/transPattern.js
@@ -1,6 +1,15 @@
 ﻿module.exports = {
     trans: function (reqArr) {
-        console.log('go');
-      return reqArr;
+        //UY
+        //var pattern = /2\d\d\d/ig;
+        // if(pattern.test(origin)) {
+        //     console.log(pattern.exec(origin));
+        //     var arr = origin.match(pattern);
+        //     var intArr = Math.min.apply(null, arr.map(Number));
+        //   } else {
+        //     console.log('no');
+        //   }
+        
+        return reqArr;
     }
-  };
+};
