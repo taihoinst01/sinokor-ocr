0ac6231c0388cc4352a77e3aa51deb00daad4096
JeongHanYoung <jhy2089@naver.com>
2019-03-11 11:31:49 +0900

    db commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,8 +2,8 @@
 // 설치시 : npm 에러 날 경우 cmd창에서 git config http.sslVerify false 수행
 var dbConfig = {
     user: process.env.NODE_ORACLEDB_USER || "koreanre",
-    password: process.env.NODE_ORACLEDB_PASSWORD || "koreanre01",
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "172.16.53.143/orcl",
+    password: process.env.NODE_ORACLEDB_PASSWORD || "koreanre02",
+    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "127.0.0.1/orcl",
     externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false,
     poolMax: 30,
     poolMin: 10
